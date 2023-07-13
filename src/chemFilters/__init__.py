d78f7a11edf141081ea0b05c811229cd269f9697
David-Araripe <david.araripe17@gmail.com>
2023-07-13 16:17:08 +0200

    fix import error in init

--- src/chemFilters/__init__.py
+++ src/chemFilters/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from .filters import RDKitFilters  # noqa F401
 from .pepfilters import PeptideFilters  # noqa F401
+from .rdkit_filters import RDKitFilters  # noqa F401
 
 __version__ = "0.0.1"
