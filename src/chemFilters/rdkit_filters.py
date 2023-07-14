1b0e090ccb4e374d0a6f8a4811de80d41fbb15ba
David-Araripe <david.araripe17@gmail.com>
2023-07-14 12:07:26 +0200

    update rdkit_filter - better exception handling

--- src/chemFilters/rdkit_filters.py
+++ src/chemFilters/rdkit_filters.py
@@ -3,7 +3,6 @@
 
 import warnings
 from functools import partial
-from itertools import product
 from multiprocessing import Pool
 from typing import List, Tuple, Union
 
@@ -12,7 +11,6 @@ import pandas as pd
 from rdkit import Chem
 from rdkit.Chem.FilterCatalog import FilterCatalog, FilterCatalogParams
 
-from .img_render import MolPlotter
 from .utils import get_catalog_match, smi_from_mol
 
 
@@ -79,8 +77,10 @@ class RdkitFilters:
 
     def get_flagging_df(self, mols: List[Union[Chem.Mol, str]]) -> pd.DataFrame:
         """Flag molecules using the defined RDKit FilterCatalogs and return a dataframe
-        with all the filters as columns and the molecules as rows. Items within the
-        dataframe will be the description of the molecular filter that was caught.
+        with all the detedcted filters as columns and the molecules as rows. Items
+        within the dataframe will be the description of the molecular filter that was
+        caught. Will also save the filter names, descriptions, and substructures as
+        attributes.
 
         Args:
             mols: list of RDKit Mol objects or SMILES strings if self._from_smi is True.
@@ -95,8 +95,7 @@ class RdkitFilters:
             )
         filter_names, descriptions, substructs = self.filter_mols(mols)
         nope = ["PAINS", "CHEMBL", "BRENK", "ALL"]  # Are collections of filters
-        columns = ["SMILES"] + [
-            c
+        columns = [c
             for c in self.available_filters
             if c not in nope and not c.startswith("CHEMBL_")
         ]
@@ -104,9 +103,14 @@ class RdkitFilters:
             list(zip(filter_names, descriptions)),
             columns=["Names", "Descriptions"],
         )
+        error_idx = []
         final_df = pd.DataFrame(columns=columns)
         for idx, row in df.iterrows():
-            label_dict = dict(zip(row["Names"], row["Descriptions"]))
+            try:
+                label_dict = dict(zip(row["Names"], row["Descriptions"]))
+            except TypeError:
+                label_dict = {}
+                error_idx.append(idx)
             final_df = pd.concat(
                 [final_df, pd.DataFrame(label_dict, index=[0])], ignore_index=True
             )
@@ -114,10 +118,16 @@ class RdkitFilters:
         for col in final_df.columns:
             final_df[col] = final_df[col].apply(lambda x: ";".join(x))
         if self._from_smi:
-            final_df["SMILES"] = mols
+            final_df.insert(0, "SMILES", mols)
         else:
             with Pool(self.n_jobs) as p:
-                final_df["SMILES"] = p.map(smi_from_mol, mols)
+                final_df.insert(0, "SMILES", p.map(smi_from_mol, mols))
+        if error_idx != []:
+            final_df.loc[error_idx, "SMILES"] = np.nan
+            warnings.warn(
+                f"Failed to get filter names and descriptions for {len(error_idx)} "
+                f"molecules in indexes: {error_idx}."
+            )
         self.filter_names = filter_names
         self.descriptions = descriptions
         self.substructs = substructs
