159d9be0b27bbd70e9827c0f87cf4ec82d9709d5
David-Araripe <david.araripe17@gmail.com>
2024-03-14 16:19:51 +0100

    Bump up version ⬆️

--- src/chemFilters/__init__.py
+++ src/chemFilters/__init__.py
@@ -4,4 +4,4 @@ from .filters.pep_filters import PeptideFilters  # noqa F401
 from .filters.rdkit_filters import RdkitFilters  # noqa F401
 from .filters.silly_filters import SillyMolSpotterFilter  # noqa F401
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
