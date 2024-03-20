aa5107a93dd863f7b01f01d69c521d54d63b4dae
David-Araripe <david.araripe17@gmail.com>
2024-03-20 15:46:11 +0100

    add testing for custom standardizing functions

--- tests/test_standardizers.py
+++ tests/test_standardizers.py
@@ -2,8 +2,20 @@ import unittest
 from pathlib import Path
 
 from rdkit import Chem
+from rdkit.Chem.MolStandardize.rdMolStandardize import ChargeParent
 
 from chemFilters.chem.standardizers import ChemStandardizer
+
+
+def pickable_std_smi_func(smi):
+    mol = Chem.MolFromSmiles(smi)
+    ChargeParent(mol)
+    return mol
+
+
+def pickable_std_mol_func(mol):
+    ChargeParent(mol)
+    return mol
 
 
 class TestChemStandardizer(unittest.TestCase):
@@ -13,12 +25,42 @@ class TestChemStandardizer(unittest.TestCase):
         self.chembl_smi_standardizer = ChemStandardizer(method="chembl", from_smi=True)
         self.chembl_mol_standardizer = ChemStandardizer(method="chembl", from_smi=False)
         self.molvs_smi_standardizer = ChemStandardizer(method="molvs", from_smi=True)
+        self.custom_smi_standardizer = ChemStandardizer(
+            method=pickable_std_smi_func, from_smi=True
+        )
+        self.custom_mol_standardizer = ChemStandardizer(
+            method=pickable_std_mol_func, from_smi=False
+        )
         self.papyrus_smi_standardizer = ChemStandardizer(
             method="papyrus", from_smi=True, small_molecule_min_mw=150
         )
         self.test_smiles = (
             (self.testroot / "resources/testSmiles.smi").read_text().splitlines()
         )
+
+    def test_custom_standardizer(self):
+        result = self.custom_smi_standardizer([self.aspirin] * 5)
+        self.assertEqual(result, ["CC(=O)Oc1ccccc1C(=O)O"] * 5)
+        result = self.custom_mol_standardizer([Chem.MolFromSmiles(self.aspirin)] * 5)
+        self.assertEqual(result, ["CC(=O)Oc1ccccc1C(=O)O"] * 5)
+        nopickle_custom_from_smi = ChemStandardizer(
+            method=lambda smi: Chem.MolFromSmiles(smi), from_smi=True
+        )
+        nopicle_custom_from_mol = ChemStandardizer(
+            method=lambda mol: ChargeParent(mol), from_smi=False
+        )
+        result = nopickle_custom_from_smi([self.aspirin] * 5)
+        self.assertEqual(result, ["CC(=O)Oc1ccccc1C(=O)O"] * 5)
+        result = nopicle_custom_from_mol([Chem.MolFromSmiles(self.aspirin)] * 5)
+        self.assertEqual(result, ["CC(=O)Oc1ccccc1C(=O)O"] * 5)
+
+    def test_canon_smiles(self):
+        stdzer = ChemStandardizer(method="canon", from_smi=True)
+        result = stdzer([self.aspirin] * 5)
+        self.assertEqual(result, ["CC(=O)Oc1ccccc1C(=O)O"] * 5)
+        stdzer = ChemStandardizer(method="canon", from_smi=False)
+        result = stdzer([Chem.MolFromSmiles(self.aspirin)] * 5)
+        self.assertEqual(result, ["CC(=O)Oc1ccccc1C(=O)O"] * 5)
 
     def test_standardizer_invalid_smiles(self):
         result = self.chembl_smi_standardizer(["Got", "some", "invalid", "smiles"])
