d81bae1f8876f862409061ed6428e3621628f8b1
David-Araripe <david.araripe17@gmail.com>
2024-03-22 12:38:50 +0100

    automatic versioning with setuptools scm

--- src/chemFilters/__init__.py
+++ src/chemFilters/__init__.py
@@ -3,5 +3,6 @@ from .filters.bloom_filters import MolbloomFilters  # noqa F401
 from .filters.pep_filters import PeptideFilters  # noqa F401
 from .filters.rdkit_filters import RdkitFilters  # noqa F401
 from .filters.silly_filters import SillyMolSpotterFilter  # noqa F401
+from .version_helper import get_version
 
-__version__ = "1.0.1"
+__version__ = get_version()
