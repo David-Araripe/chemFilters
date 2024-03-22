3184f65cb136badcbc64464216ebb0a73931da27
David-Araripe <david.araripe17@gmail.com>
2024-03-22 13:49:25 +0100

    fix toml & init imports to work with setuptools scm

--- src/chemFilters/__init__.py
+++ src/chemFilters/__init__.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
-from .filters.bloom_filters import MolbloomFilters  # noqa F401
-from .filters.pep_filters import PeptideFilters  # noqa F401
-from .filters.rdkit_filters import RdkitFilters  # noqa F401
-from .filters.silly_filters import SillyMolSpotterFilter  # noqa F401
+try:
+    from .filters.bloom_filters import MolbloomFilters  # noqa F401
+    from .filters.pep_filters import PeptideFilters  # noqa F401
+    from .filters.rdkit_filters import RdkitFilters  # noqa F401
+    from .filters.silly_filters import SillyMolSpotterFilter  # noqa F401
+except ImportError:
+    pass
 from .version_helper import get_version
 
 __version__ = get_version()
