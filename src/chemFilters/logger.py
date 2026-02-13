8d0e7ee5cb7f921f3e2969ed08d6e94fdffd5cd9
David-Araripe <david.araripe17@gmail.com>
2026-02-13 14:54:20 +0100

    change setup_logger not to call logger.remove() as it was breaking the loguru logging of other applications

--- src/chemFilters/logger.py
+++ src/chemFilters/logger.py
@@ -1,17 +1,27 @@
 # -*- coding: utf-8 -*-
+
 import sys
 
 from loguru import logger
 
+COLORFUL_FORMAT = (
+    "<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}"
+    "</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan>"
+    " - <level>{message}</level>"
+)
+
 
 def setup_logger(level="INFO"):
-    colorful_format = (
-        "<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}"
-        "</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan>"
-        " - <level>{message}</level>"
+    """Add a colorful log handler filtered to chemFilters messages only.
+
+    Does not remove existing handlers. Returns the handler ID so it can be
+    removed later with ``logger.remove(handler_id)``.
+    """
+    handler_id = logger.add(
+        sys.stderr,
+        format=COLORFUL_FORMAT,
+        level=level,
+        filter="chemFilters",
+        colorize=True,
     )
-    logger.remove()
-    logger.add(sys.stderr, format=colorful_format, level=level, colorize=True)
-
-
-setup_logger()
+    return handler_id
